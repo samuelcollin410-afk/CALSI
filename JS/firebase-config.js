8ac8e2ab309a43283da118bf0ab6ed51f4e6b88b
samuelcollin410-afk <samuelcollin410@gmail.com>
2026-08-24 23:17:09 -0400

    Configure Firebase for CALSI

--- JS/firebase-config.js
+++ JS/firebase-config.js
@@ -1,14 +1,11 @@
-// 1. Create a Firebase project: https://console.firebase.google.com/
-// 2. Add a Web App.
-// 3. Enable Authentication > Google.
-// 4. Create a Firestore database.
-// 5. Add your GitHub Pages domain to Authentication > Settings > Authorized domains.
-// 6. Replace the values below.
+// Firebase web app configuration for CALSI.
+// Google Authentication and Firestore must also be enabled in the Firebase console.
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCl-OkP8I2ioKRukT3rRTunY3cENxyc8-k",
+  authDomain: "calsi-cbe65.firebaseapp.com",
+  projectId: "calsi-cbe65",
+  storageBucket: "calsi-cbe65.firebasestorage.app",
+  messagingSenderId: "561502440480",
+  appId: "1:561502440480:web:b17437a6c78fcc38f14045",
+  measurementId: "G-X842HQRNY0"
 };
